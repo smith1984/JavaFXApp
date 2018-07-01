54e764b6a373048f286e58a9381f8266a28ab45c
Maksim Petrov <smith150384@gmail.com>
2018-07-01 15:41:32 +0300

    New app

---
+++ src/main/java/ru/smith/Main.java
@@ -0,0 +1,22 @@
+package ru.smith;
+
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Group group = new Group();
+        Scene scene = new Scene(group, 300, 400);
+
+        primaryStage.setTitle("Hello World!");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+}
